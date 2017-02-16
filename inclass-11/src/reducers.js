46fbdcb4d3cdb6ee2b55f42f1a184099549f988c
lizkGithub <lizhaokang2008@126.com>
2017-02-16 17:00:14 -0600

    add inclass11

---
+++ inclass-11/src/reducers.js
@@ -0,0 +1,42 @@
+
+const Reducer = (state =  {
+	nextId: 2,
+	todoItems: [
+	    {id: 0, text: "This is an item", done: false},
+	    {id: 1, text: "Another item", done: false}
+	]
+}, action) => {
+	switch(action.type) {
+		case 'ADD_TODO':
+			// IMPLEMENT ME
+			return {
+				nextId: state.nextId + 1,
+				todoItems: [
+					...state.todoItems,
+					{id: state.nextId, text: action.text, done: false}
+				]
+
+			}
+		case 'REMOVE_TODO':
+			// IMPLEMENT ME
+			return {
+				nextId: state.nextId - 1,
+				todoItems: state.todoItems.filter(({id}) => (id != action.id))
+			}
+		case 'TOGGLE_TODO':
+			// IMPLEMENT ME
+			return {
+				nextId: state.nextId,
+				todoItems: state.todoItems.map((item) => {
+					if (item.id === action.id) {
+						return Object.assign({}, item, {done: !item.done})
+					}
+					return item
+				})
+			}
+		default: 
+			return state
+	}
+}
+
+export default Reducer

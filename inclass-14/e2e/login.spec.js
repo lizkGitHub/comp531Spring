06da4c9a46543d676f2d625a0713ab344eca32f1
lizkGithub <lizhaokang2008@126.com>
2017-03-01 21:55:16 -0600

    inclass14

---
+++ inclass-14/e2e/login.spec.js
@@ -0,0 +1,58 @@
+import { expect } from 'chai'
+import { go, sleep, findId, findCSS, By } from './selenium'
+import common from './common'
+
+describe('Test Dummy Server Example Page', () => {
+
+    const preamble = 'you are logged in as'
+
+    before('should log in', (done) => {
+        go().then(common.login).then(done)
+    })
+
+    it('should log in as the test user', (done) => {
+        sleep(500)
+            .then(findId('message').getText()
+                .then(text => {
+                    expect(text.indexOf(preamble)).to.equal(0)
+                })
+                .then(done))
+    })
+
+    it("Update the headline and verify the change", (done) => {
+        // IMPLEMENT ME
+        // find the headline input
+        // .sendKeys(new headline message)
+        // verify the headline is updated
+        // .sendKeys(the old headline message)
+        // verify the headline is updated
+        var oldHeadline = 'This is old headline!'
+        var newHeadline = 'This is new headline!'
+        var username = common.creds.username;
+        var pre = `you are logged in as ${username} `
+        sleep(500)
+            .then(findId('newHeadline').sendKeys(newHeadline))
+            .then(findId('headline').click())
+            .then(sleep(2000))
+            // verify the headline is updated
+            .then(findId('message').getText()
+                .then(text => {
+                    expect(text).to.equal(pre + '"' + newHeadline + '"')
+                }))
+            // .sendKeys(the old headline message)
+            // verify the headline is updated
+            .then(findId('oldHeadline').sendKeys(oldHeadline))
+            .then(findId('headline').click())
+            .then(sleep(2000))
+            // verify the headline is updated
+            .then(findId('message').getText()
+                .then(text => {
+                    expect(text).to.equal(pre + '"' + oldHeadline + '"')
+                }))
+        done()
+    })
+
+    after('should log out', (done) => {
+        common.logout().then(done)
+    })
+})
